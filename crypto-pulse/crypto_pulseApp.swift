924696fa69012e4b1d881602d6c14b1dca0d1830
Patrik Roh <patrik.roh@wbpo.sk>
2026-01-27 14:51:18 +0100

    Initial Commit

---
+++ crypto-pulse/crypto_pulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  crypto_pulseApp.swift
+//  crypto-pulse
+//
+//  Created by Patrick Edge on 27/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct crypto_pulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
